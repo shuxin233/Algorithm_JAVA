a0bcd7dcea315010a5ab0072488b452027e6384d
lsx01 <1120111949@qq.com>
2021-01-10 21:17:40 +0800

    2021/1/10

---
+++ src/Array/a01.java
@@ -0,0 +1,27 @@
+package Array;
+
+public class a01 {
+    public static void main(String[] args) {
+        int[] nums =new int[]{1,1,2,3};
+        int a=removeDuplicates(nums);
+        System.out.println(a);
+        for (int temp:
+             nums) {
+            System.out.println(temp);
+        }
+    }
+
+    public static int  removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=0;j<nums.length;j++) {
+        if(nums[j]!=nums[i]){
+            nums[++i]=nums[j];
+        }
+        }
+        return ++i;
+    }
+
+
+}
+
+
--- src/Array/aa.java
+++ src/Array/aa.java
@@ -2,6 +2,8 @@ package Array;
 
 public class aa {
     public static void main(String[] args) {
-        System.out.println("main");
+
+
+        
     }
 }
